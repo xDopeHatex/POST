4f5ae205691221e61874f5f4c4550e2a14031603
xDopeHatex <xgorokhovx@gmail.com>
2022-10-12 10:22:04 +0400

    post project v.1.0

---
+++ JS/script.js
@@ -0,0 +1,262 @@
+"use strict";
+
+const getRandom = function () {
+  return Math.trunc(Math.random() * 9999999999) + 1;
+};
+
+// Add storage
+let store = [];
+
+//Vars
+
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const taskInput = document.querySelector("#task-input");
+
+// Load Tasks
+
+async function getPostsFromServer() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const rawPostsArr = await response.json();
+
+  store = await rawPostsArr.map((item) => {
+    return {
+      innerId: `${getRandom()}`,
+      id: item.title,
+      body: item.body,
+    };
+  });
+  store?.forEach((post) => {
+    template(post);
+  });
+  console.log(store[0]);
+  console.log(store[1]);
+}
+//getPostsFromServer();
+
+// Creating templates
+
+const templateLiElemForAdd = function (rawAddPost) {
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = `flex items-center rounded-xl justify-between p-3 group bg-white`;
+
+  //Add id
+
+  li.setAttribute("id", `${rawAddPost.innerId}`);
+
+  li.innerHTML = `<div class="flex space-x-2">
+ <label
+   >${rawAddPost.body}</label
+ >
+</div>
+<div id="3" class="flex gap-4">
+           <button
+             id="2.5"
+             class="reduct-item flex items-center justify-center h-6 w-6 z-0"
+           >
+             <img
+               src="../img/pencil.svg"
+               id="1.5"
+               class="group-hover:scale-100 bg-pink-300 rounded-md scale-0 z-0 transition-all duration-200  active:bg-pink-600 active:translate-y-0.5"
+               alt=""
+             />
+           </button>
+ <button class="delete-item flex items-center justify-center h-6 w-6 z-0">
+   <img
+     src="../img/delete.svg"
+     class=" group-hover:scale-100 bg-pink-300 rounded-md scale-0 z-0 transition-all duration-200 d active:bg--600 active:translate-y-0.5 "
+     alt=""
+   />
+ </button>
+</div>`;
+  //taskList.appendChild(li);
+
+  const theFirstChild = taskList.firstChild;
+
+  taskList.insertBefore(li, theFirstChild);
+};
+
+const templateLiElemForStartingRedaction = function (rawAddPost) {};
+
+// Add task event
+
+form.addEventListener("submit", addPostToStore);
+
+// Add Task
+
+async function addPostToStore(e) {
+  if (taskInput.value === "") {
+    alert("It seems that you forgot to add a post, bro");
+  } else {
+    e.preventDefault();
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: `${taskInput.value}`,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    console.log(response);
+    let rawAddPost = await response.json();
+    // Random Id
+
+    const d = new Date();
+    let ms = d.valueOf();
+    rawAddPost.innerId = `${ms}`;
+
+    console.log(rawAddPost);
+
+    console.log(store.length);
+
+    store.splice(store.length, 0, rawAddPost);
+
+    templateLiElemForAdd(rawAddPost);
+
+    e.preventDefault();
+  }
+
+  console.log(store[0]);
+  e.preventDefault();
+  taskInput.value = "";
+  console.log(store);
+}
+
+//Remove task event
+taskList.addEventListener(`click`, removeTask);
+
+async function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm(`Are you sure, bro?`)) {
+      await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+        method: "DELETE",
+      });
+
+      const id = e.target.parentNode.parentNode.parentNode.id;
+
+      const index = store.map((object) => object.innerId).indexOf(id);
+
+      console.log(index);
+
+      store.splice(index, 1);
+
+      console.log(store);
+
+      e.target.parentElement.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Reduct task event
+taskList.addEventListener(`click`, reductTask);
+
+function reductTask(e) {
+  if (e.target.parentElement.classList.contains("reduct-item")) {
+    const id = e.target.parentNode.parentNode.parentNode.id;
+
+    let currentTask = store.find((item) => item.innerId == id);
+
+    console.log(currentTask);
+    // Create li element
+    let li = document.getElementById(`${id}`);
+
+    li.innerHTML = "";
+
+    li.innerHTML = `<div class="flex space-x-2">
+    <label class="flex space-x-2 items-center"
+      ><form class="flex space-x-2" type="submit" id="reductForm"><input class="bg-slate-200" type="text" name="text" id="reductText" value="${currentTask.body}"/></form></label
+    >
+  </div>
+  <div id="3" class="flex gap-4">
+  
+    <button
+    id="2.7"
+    class="done-item flex items-center justify-center h-6 w-6 z-0 type="submit"
+  >
+    <img
+      src="../img/done.svg"
+      id="1.7"
+      class="group-hover:scale-100 bg-pink-300 rounded-md scale-0 z-0 transition-all duration-200 d active:bg-pink-600 active:translate-y-0.5 "
+      alt=""
+    />
+  </button>
+    <button class="delete-item flex items-center justify-center h-6 w-6 z-0">
+      <img
+        src="../img/delete.svg"
+        class=" group-hover:scale-100 bg-pink-300 rounded-md scale-0 z-0 transition-all duration-200 d active:bg-pink-600 active:translate-y-0.5 "
+        alt=""
+      />
+    </button>
+  </div>`;
+
+    e.preventDefault();
+  }
+}
+
+//Done reduct text
+
+taskList.addEventListener(`click`, doneTask);
+
+async function doneTask(e) {
+  if (e.target.parentElement.classList.contains("done-item")) {
+    const id = e.target.parentNode.parentNode.parentNode.id;
+
+    let currentTask = store.find((item) => item.innerId == id);
+
+    // changed text send to object
+    const taskInputCurrent = document.querySelector("#reductText");
+
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1",
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          body: taskInputCurrent.value,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const post = await response.json();
+
+    console.log(post);
+
+    let li = document.getElementById(`${id}`);
+
+    li.innerHTML = "";
+
+    li.innerHTML = `<div class="flex space-x-2">
+    <label
+      >${post.body}</label
+    >
+   </div>
+   <div id="3" class="flex gap-4">
+              <button
+                id="2.5"
+                class="reduct-item flex items-center justify-center h-6 w-6 z-0"
+              >
+                <img
+                  src="../img/pencil.svg"
+                  id="1.5"
+                  class="group-hover:scale-100 bg-pink-300 rounded-md scale-0 z-0 transition-all duration-200  active:bg-pink-600 active:translate-y-0.5"
+                  alt=""
+                />
+              </button>
+    <button class="delete-item flex items-center justify-center h-6 w-6 z-0">
+      <img
+        src="../img/delete.svg"
+        class=" group-hover:scale-100 bg-pink-300 rounded-md scale-0 z-0 transition-all duration-200 d active:bg--600 active:translate-y-0.5 "
+        alt=""
+      />
+    </button>
+   </div>`;
+  }
+}
